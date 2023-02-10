58dc22a09b587395be268b2fddef1aa9a94f6564
Dmitry Agurkov <munnharpa@gmail.com>
2023-02-10 14:10:48 +0300

    initial prisma schema

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './api/app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -14,12 +14,12 @@ async function bootstrap() {
     }),
   );
 
-  const config = new DocumentBuilder()
-    .setTitle('Library service')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('Library service')
+  //   .setVersion('1.0')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('', app, document);
 
   await app.listen(process.env.PORT || 4000);
 }
