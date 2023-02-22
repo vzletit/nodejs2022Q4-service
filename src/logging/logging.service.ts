b46258a4ff41a28d1318ea4ea4edb851d52dfe19
Dmitry Agurkov <munnharpa@gmail.com>
2023-02-22 14:43:59 +0300

    fix: logger status code

--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -1,17 +1,24 @@
-import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggingService implements NestMiddleware {
   private logger = new Logger('HTTP');
-  use(req: Request, res: Response, next: NextFunction) {
-    this.logger.log(
-      'Logging HTTP request: ' + req.method,
-      req.url,
-      req.query,
-      req.body,
-      res.statusCode,
-    );
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, path: url, query, body } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    response.on('close', () => {
+      const { statusCode } = response;
+
+      this.logger.log(
+        `${method}: ${url}, query: ${JSON.stringify(
+          query,
+        )}, body: ${JSON.stringify(body)} - ${statusCode} `,
+      );
+    });
+
     next();
   }
 }
